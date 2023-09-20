5f2bfe39ffd0a30eaf51248d54509abf7df6c9aa
masudalom1997 <alom2814@gmail.com>
2023-09-20 19:49:52 -0400

    first commit

---
+++ src/main/java/com/practicing_framework_myself/FRAMEWORK/App.java
@@ -0,0 +1,13 @@
+package com.practicing_framework_myself.FRAMEWORK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/PracticingTutorialsninja/com/RegisterTest.java
@@ -0,0 +1,10 @@
+package com.PracticingTutorialsninja.com;
+
+public class RegisterTest {
+
+	public static void main(String[] args) {
+		System.out.println("Panda is my tacher");
+
+	}
+
+}
